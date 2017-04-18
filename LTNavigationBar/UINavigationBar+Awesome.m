7df7c4b04bd9ead1e0c18a0033ce9c295fba1d26
YangQiangGit <591801530@qq.com>
2017-04-18 15:21:31 +0800

    no message

---
+++ LTNavigationBar/UINavigationBar+Awesome.m
@@ -0,0 +1,78 @@
+//
+//  UINavigationBar+Awesome.m
+//  LTNavigationBar
+//
+//  Created by ltebean on 15-2-15.
+//  Copyright (c) 2015 ltebean. All rights reserved.
+//
+
+#import "UINavigationBar+Awesome.h"
+#import <objc/runtime.h>
+
+#define SYSTEM_VERSION_LESS_THAN(v) ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
+
+@implementation UINavigationBar (Awesome)
+static char overlayKey;
+#pragma mark --- Setter && Getter ---- //使用runtime为类别添加属性
+- (UIView *)overlay
+{
+    return objc_getAssociatedObject(self, &overlayKey);
+}
+
+- (void)setOverlay:(UIView *)overlay
+{
+    objc_setAssociatedObject(self, &overlayKey, overlay, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+- (void)lt_setBackgroundColor:(UIColor *)backgroundColor
+{
+    if (!self.overlay) {
+        [self setBackgroundImage:[UIImage new] forBarMetrics:UIBarMetricsDefault];
+        self.overlay = [[UIView alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth(self.bounds), CGRectGetHeight(self.bounds) + 20)];
+        self.overlay.userInteractionEnabled = NO;
+        self.overlay.autoresizingMask = UIViewAutoresizingFlexibleWidth;    // Should not set `UIViewAutoresizingFlexibleHeight`
+        [[self.subviews firstObject] insertSubview:self.overlay atIndex:0];
+    }
+    self.overlay.backgroundColor = backgroundColor;
+}
+
+- (void)lt_setTranslationY:(CGFloat)translationY
+{
+    self.transform = CGAffineTransformMakeTranslation(0, translationY);
+}
+
+- (void)lt_setElementsAlpha:(CGFloat)alpha
+{
+    //遍历左侧Item
+    [[self valueForKey:@"_leftViews"] enumerateObjectsUsingBlock:^(UIView *view, NSUInteger i, BOOL *stop) {
+        view.alpha = alpha;
+    }];
+    //遍历右侧Item
+    [[self valueForKey:@"_rightViews"] enumerateObjectsUsingBlock:^(UIView *view, NSUInteger i, BOOL *stop) {
+        view.alpha = alpha;
+    }];
+    
+    UIView *titleView = [self valueForKey:@"_titleView"];
+    titleView.alpha = alpha;
+    
+//    when viewController first load, the titleView maybe nil
+    //遍历NavigationBar上的所有子视图 这里你可以打断点自己看视图层次结构
+    [[self subviews] enumerateObjectsUsingBlock:^(UIView *obj, NSUInteger idx, BOOL *stop) {
+        //承载Title的视图Item
+        if ([obj isKindOfClass:NSClassFromString(@"UINavigationItemView")]) {
+            obj.alpha = alpha;
+        }
+        //左侧自带的返回按钮
+        if ([obj isKindOfClass:NSClassFromString(@"_UINavigationBarBackIndicatorView")]) {
+            obj.alpha = 0;
+        }
+    }];
+}
+
+- (void)lt_reset
+{
+    [self setBackgroundImage:nil forBarMetrics:UIBarMetricsDefault];
+    [self.overlay removeFromSuperview];
+    self.overlay = nil;
+}
+
+@end
